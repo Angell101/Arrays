a5d46c7ad3233edb8a5059287e71f3aeb7dc5215
Angell <Sa1Lo_xd@vk.cpm>
2023-12-29 17:12:23 +1100

    commit 3

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,86 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        task1();// задача 1
+        task2();// задача 2
+        task3();// задача 3
+        //task4();// задача 4
 
+    }
 
-        for (int i = 1; i <= 5; i++) {
+    public static void task1() {
+        System.out.println("Задание 1");
 
-            System.out.println("i = " + i);
+        int[] numbers = new int [] {1, 2, 3};
+        double[] numbers1 = {1.57, 7.654, 9.986};
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i]);
+            if (i != numbers.length -1);
+            {
+                System.out.print(", ");
+            }
         }
+        System.out.println();
+
+        for (int i = 0; i < numbers1.length; i++) {
+            System.out.print(numbers1[i]);
+            if ( i != numbers.length -1);
+            {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+    }
+    public static void task2() {
+        System.out.println("Задание 2");
+
+        int [] arr1 = new int[3];
+        arr1[0] = 1;
+        arr1[1] = 2;
+        arr1[2] = 3;
+
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.print(arr1[i]);
+            if (i != arr1.length -1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        double [] arr2 = {1.57, 7.654, 9.986};
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i]);
+            if (i != arr2.length -1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void task3() {
+        System.out.println("задание 3");
+
+        int [] arr1 = new int[3];
+        arr1[0] = 3;
+        arr1[1] = 2;
+        arr1[2] = 1;
+
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.print(arr1[i]);
+            if (i != arr1.length -1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        double [] arr2 = {9.986, 7.654, 1.57};
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i]);
+            if (i != arr2.length -1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void task4() {
+
     }
 }
